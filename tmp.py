314f670fcc5e7af6aba0a9cbdc6325f854ebe924
chlgusdnd <chw030703@gmail.com>
2023-12-09 13:17:59 +0900

    add do, re, mi, pr

--- tmp.py
+++ tmp.py
@@ -21,11 +21,31 @@ with handsModule.Hands(static_image_mode=False, min_detection_confidence=0.7, mi
 
         overlay = frame1.copy()
         cv2.line(overlay, (0, 650), (1000, 650), (0, 0, 0), 2)
-        # In case the system sees multiple hands this if statment deals with that and produces another hand overlay
+
+        gesture_text = 'Cant found hand'
         if results.multi_hand_landmarks != None:
             for handLandmarks in results.multi_hand_landmarks:
                 drawingModule.draw_landmarks(frame1, handLandmarks, handsModule.HAND_CONNECTIONS)
-
+                finger1_right_y = 0
+                finger1_right_x = 0
+                finger2_right_y = 0
+                finger2_right_x = 0
+                finger3_right_y = 0
+                finger3_right_x = 0
+                finger4_right_y = 0
+                finger4_right_x = 0
+                finger5_right_y = 0
+                finger5_right_x = 0
+                finger1_left_x = 0
+                finger1_left_y = 0
+                finger2_left_x = 0
+                finger2_left_y = 0
+                finger3_left_x = 0
+                finger3_left_y = 0
+                finger4_left_x = 0
+                finger4_left_y = 0
+                finger5_left_x = 0
+                finger5_left_y = 0
                 for point in handsModule.HandLandmark:
                     normalizedLandmark = handLandmarks.landmark[point]
                     #pixelCoordinatesLandmark= drawingModule._normalized_to_pixel_coordinates(normalizedLandmark.x, normalizedLandmark.y, 1000, 1000)
@@ -33,7 +53,9 @@ with handsModule.Hands(static_image_mode=False, min_detection_confidence=0.7, mi
                         right_x = int(normalizedLandmark.x * 1000)
                         right_y = int(normalizedLandmark.y * 1000)
                         if right_y > 650:
-                            print(point)
+                            # print(point)
+                            finger1_right_y = right_y
+                            finger1_right_x = right_x
                             # print(pixelCoordinatesLandmark)
                             # print(normalizedLandmark)
                             right_x -= 30
@@ -57,30 +79,68 @@ with handsModule.Hands(static_image_mode=False, min_detection_confidence=0.7, mi
                             cv2.line(overlay, (right_x - 30, right_y - 175), (right_x - 30, right_y + 225), (0, 0, 0),2)
                             cv2.line(overlay, (right_x + 30, right_y - 175), (right_x + 30, right_y + 225), (0, 0, 0),2)
                             frame1 = cv2.addWeighted(overlay, 0.4, frame1, 1, 0)
+                        else:
+                            finger1_right_y = right_y
+                            finger1_right_x = right_x
+
+                    if point == 8:
+                        print("dddd")
+                        if int(normalizedLandmark.y * 1000) > finger1_right_y:
+                            finger2_right_x = int(normalizedLandmark.x * 1000)
+                            finger2_right_y = int(normalizedLandmark.y * 1000)
+                            print("3", finger2_right_x, finger2_right_y)
+                        else:
+                            finger2_left_x = int(normalizedLandmark.x * 1000)
+                            finger2_left_y = int(normalizedLandmark.y * 1000)
+                    if point == 12:
+                        if int(normalizedLandmark.y * 1000) > finger1_right_y:
+                            finger3_right_x = int(normalizedLandmark.x * 1000)
+                            finger3_right_y = int(normalizedLandmark.y * 1000)
+                        else:
+                            finger3_left_x = int(normalizedLandmark.x * 1000)
+                            finger3_left_y = int(normalizedLandmark.y * 1000)
+                    if point == 16:
+                        if int(normalizedLandmark.y * 1000) > finger1_right_y:
+                            finger4_right_x = int(normalizedLandmark.x * 1000)
+                            finger4_right_y = int(normalizedLandmark.y * 1000)
+                        else:
+                            finger4_left_x = int(normalizedLandmark.x * 1000)
+                            finger4_left_y = int(normalizedLandmark.y * 1000)
+                    if point == 20:
+                        if int(normalizedLandmark.y * 1000) > finger1_right_y:
+                            finger5_right_x = int(normalizedLandmark.x * 1000)
+                            finger5_right_y = int(normalizedLandmark.y * 1000)
+                        else:
+                            finger5_left_x = int(normalizedLandmark.x * 1000)
+                            finger5_left_y = int(normalizedLandmark.y * 1000)
+
+                    print("1",finger1_right_y, finger1_right_x)
+                    print("2",finger5_right_y, finger5_right_x)
+
+                    if finger1_right_y + 175 < finger5_right_y < finger1_right_y + 255:
+                        if finger1_right_x - 30 < finger5_right_x < finger1_right_x + 30:
+                            print("do")
+                            gesture_text = 'do'
+                            cv2.rectangle(frame1, (right_x - 30, right_y - 175), (right_x + 30, right_y + 225), (0,0,255), 3)
+                    elif finger1_right_y + 125 < finger4_right_y < finger1_right_y + 175:
+                        if finger1_right_x - 30 < finger4_right_x < finger1_right_x + 30:
+                            print("re")
+                            gesture_text = 're'
+                            cv2.rectangle(frame1, (right_x - 30, right_y - 175), (right_x + 30, right_y + 175), (0, 0, 255), 3)
+                    elif finger1_right_y + 75 < finger3_right_y < finger1_right_y + 125:
+                        if finger1_right_x - 30 < finger3_right_x < finger1_right_x + 30:
+                            print("re")
+                            gesture_text = 'mi'
+                            cv2.rectangle(frame1, (right_x - 30, right_y - 175), (right_x + 30, right_y + 125), (0, 0, 255), 3)
+                    elif finger1_right_y + 25 < finger2_right_y < finger1_right_y + 75:
+                        if finger1_right_x - 30 < finger2_right_x < finger1_right_x + 30:
+                            gesture_text = 'pa'
+                            cv2.rectangle(frame1, (right_x - 30, right_y - 175), (right_x + 30, right_y + 75), (0, 0, 255), 3)
 
 
-
-
-
-                for hand_landmarks in results.multi_hand_landmarks:
-                    finger1_x = int(hand_landmarks.landmark[4].x * 1000)
-                    finger2_x = int(hand_landmarks.landmark[8].x * 1000)
-                    finger3_x = int(hand_landmarks.landmark[12].x * 1000)
-                    finger4_x = int(hand_landmarks.landmark[16].x * 1000)
-                    finger5_x = int(hand_landmarks.landmark[20].x * 1000)
-
-                    finger1_y = int(hand_landmarks.landmark[4].y * 1000)
-                    finger2_y = int(hand_landmarks.landmark[8].y * 1000)
-                    finger3_y = int(hand_landmarks.landmark[12].y * 1000)
-                    finger4_y = int(hand_landmarks.landmark[16].y * 1000)
-                    finger5_y = int(hand_landmarks.landmark[20].y * 1000)
-
-
-
-
-
-
-
+        cv2.putText(frame1, text='dd: {}'.format(gesture_text)
+                    , org=(10, 30), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale=1, color=(0, 0, 255), thickness=2)
 
         # Below shows the current frame to the desktop
         cv2.imshow("Frame", frame1);
