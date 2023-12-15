033b47fa7b006b8e9faf4a11f4aa2768075df1d8
chlgusdnd <chw030703@gmail.com>
2023-12-15 23:55:43 +0900

    들여쓰기 수정

--- instrument.py
+++ instrument.py
@@ -140,56 +140,57 @@ with handsModule.Hands(static_image_mode=False, min_detection_confidence=0.7, mi
                                 finger5_left_x = int(normalizedLandmark.x * 1000)
                                 finger5_left_y = int(normalizedLandmark.y * 1000)
 
-                        if vol > 90:
-                            if finger1_right_y + 175 < finger5_right_y < finger1_right_y + 255:
-                                if finger1_right_x - 30 < finger5_right_x < finger1_right_x + 30:
-                                    gesture_text = 'do'
-                                    # cv2.rectangle(overlay, (finger1_right_x - 60, finger1_right_y - 175), (finger1_right_x, finger1_right_y + 225), (0,0,255), 3)
-                                    cv2.rectangle(frame1, (finger1_right_x - 60, finger1_right_y - 175),
-                                                  (finger1_right_x, finger1_right_y + 225), (0, 0, 255), 3)
-                            elif finger1_right_y + 125 < finger4_right_y < finger1_right_y + 175:
-                                if finger1_right_x - 30 < finger4_right_x < finger1_right_x + 30:
-                                    gesture_text = 're'
-                                    # cv2.rectangle(overlay, (finger1_right_x - 60, finger1_right_y - 175), (finger1_right_x , finger1_right_y + 175), (0,0,255), 3)
-                                    cv2.rectangle(frame1, (finger1_right_x - 60, finger1_right_y - 175),
-                                                  (finger1_right_x, finger1_right_y + 175), (0, 0, 255), 3)
-                            elif finger1_right_y + 75 < finger3_right_y < finger1_right_y + 125:
-                                if finger1_right_x - 30 < finger3_right_x < finger1_right_x + 30:
-                                    gesture_text = 'mi'
-                                    # cv2.rectangle(overlay, (finger1_right_x - 60, finger1_right_y - 175), (finger1_right_x, finger1_right_y + 125), (0,0,255), 3)
-                                    cv2.rectangle(frame1, (finger1_right_x - 60, finger1_right_y - 175),
-                                                  (finger1_right_x, finger1_right_y + 125), (0, 0, 255), 3)
-                            elif finger1_right_y + 25 < finger2_right_y < finger1_right_y + 75:
-                                if finger1_right_x - 30 < finger2_right_x < finger1_right_x + 30:
-                                    gesture_text = 'pa'
-                                    # cv2.rectangle(overlay, (finger1_right_x - 60, finger1_right_y - 175), (finger1_right_x, finger1_right_y + 75), (0,0,255), 3)
-                                    cv2.rectangle(frame1, (finger1_right_x - 60, finger1_right_y - 175),
-                                                  (finger1_right_x, finger1_right_y + 75), (0, 0, 255), 3)
+                if vol>90:
+                    if finger1_right_y + 175 < finger5_right_y < finger1_right_y + 255:
+                        if finger1_right_x - 30 < finger5_right_x < finger1_right_x + 30:
+                            gesture_text = 'do'
+                            # cv2.rectangle(overlay, (finger1_right_x - 60, finger1_right_y - 175), (finger1_right_x, finger1_right_y + 225), (0,0,255), 3)
+                            cv2.rectangle(frame1, (finger1_right_x - 60, finger1_right_y - 175),
+                                          (finger1_right_x, finger1_right_y + 225), (0, 0, 255), 3)
+                    elif finger1_right_y + 125 < finger4_right_y < finger1_right_y + 175:
+                        if finger1_right_x - 30 < finger4_right_x < finger1_right_x + 30:
+                            gesture_text = 're'
+                            # cv2.rectangle(overlay, (finger1_right_x - 60, finger1_right_y - 175), (finger1_right_x , finger1_right_y + 175), (0,0,255), 3)
+                            cv2.rectangle(frame1, (finger1_right_x - 60, finger1_right_y - 175),
+                                          (finger1_right_x, finger1_right_y + 175), (0, 0, 255), 3)
+                    elif finger1_right_y + 75 < finger3_right_y < finger1_right_y + 125:
+                        if finger1_right_x - 30 < finger3_right_x < finger1_right_x + 30:
+                            gesture_text = 'mi'
+                            # cv2.rectangle(overlay, (finger1_right_x - 60, finger1_right_y - 175), (finger1_right_x, finger1_right_y + 125), (0,0,255), 3)
+                            cv2.rectangle(frame1, (finger1_right_x - 60, finger1_right_y - 175),
+                                          (finger1_right_x, finger1_right_y + 125), (0, 0, 255), 3)
+                    elif finger1_right_y + 25 < finger2_right_y < finger1_right_y + 75:
+                        if finger1_right_x - 30 < finger2_right_x < finger1_right_x + 30:
+                            gesture_text = 'pa'
+                            # cv2.rectangle(overlay, (finger1_right_x - 60, finger1_right_y - 175), (finger1_right_x, finger1_right_y + 75), (0,0,255), 3)
+                            cv2.rectangle(frame1, (finger1_right_x - 60, finger1_right_y - 175),
+                                          (finger1_right_x, finger1_right_y + 75), (0, 0, 255), 3)
 
-                            elif finger1_right_y - 25 < finger5_left_y < finger1_right_y + 25:
-                                if finger1_right_x - 30 < finger5_left_x < finger1_right_x + 30:
-                                    gesture_text = 'sol'
-                                    # cv2.rectangle(overlay, (finger1_right_x - 60, finger1_right_y - 175), (finger1_right_x, finger1_right_y + 25), (0,0,255), 3)
-                                    cv2.rectangle(frame1, (finger1_right_x - 60, finger1_right_y - 175),
-                                                  (finger1_right_x, finger1_right_y + 25), (0, 0, 255), 3)
-                            elif finger1_right_y - 50 < finger4_left_y < finger1_right_y - 25:
-                                if finger1_right_x - 30 < finger5_left_x < finger1_right_x + 30:
-                                    gesture_text = 'la'
-                                    # cv2.rectangle(overlay, (finger1_right_x - 60, finger1_right_y - 175), (finger1_right_x, finger1_right_y + 25), (0,0,255), 3)
-                                    cv2.rectangle(frame1, (finger1_right_x - 60, finger1_right_y - 175),
-                                                  (finger1_right_x, finger1_right_y - 25), (0, 0, 255), 3)
-                            elif finger1_right_y - 75 < finger5_left_y < finger1_right_y - 50:
-                                if finger1_right_x - 30 < finger5_left_x < finger1_right_x + 30:
-                                    gesture_text = 'ti'
-                                    # cv2.rectangle(overlay, (finger1_right_x - 60, finger1_right_y - 175), (finger1_right_x, finger1_right_y + 25), (0,0,255), 3)
-                                    cv2.rectangle(frame1, (finger1_right_x - 60, finger1_right_y - 175),
-                                                  (finger1_right_x, finger1_right_y - 50), (0, 0, 255), 3)
-                            elif finger1_right_y - 125 < finger5_left_y < finger1_right_y - 75:
-                                if finger1_right_x - 30 < finger5_left_x < finger1_right_x + 30:
-                                    gesture_text = 'do2'
-                                    # cv2.rectangle(overlay, (finger1_right_x - 60, finger1_right_y - 175), (finger1_right_x, finger1_right_y + 25), (0,0,255), 3)
-                                    cv2.rectangle(frame1, (finger1_right_x - 60, finger1_right_y - 175),
-                                                  (finger1_right_x, finger1_right_y - 75), (0, 0, 255), 3)
+                    elif finger1_right_y - 25 < finger5_left_y < finger1_right_y + 25:
+                        if finger1_right_x - 30 < finger5_left_x < finger1_right_x + 30:
+                            gesture_text = 'sol'
+                            # cv2.rectangle(overlay, (finger1_right_x - 60, finger1_right_y - 175), (finger1_right_x, finger1_right_y + 25), (0,0,255), 3)
+                            cv2.rectangle(frame1, (finger1_right_x - 60, finger1_right_y - 175),
+                                          (finger1_right_x, finger1_right_y + 25), (0, 0, 255), 3)
+                    elif finger1_right_y - 50 < finger4_left_y < finger1_right_y - 25:
+                        if finger1_right_x - 30 < finger5_left_x < finger1_right_x + 30:
+                            gesture_text = 'la'
+                            # cv2.rectangle(overlay, (finger1_right_x - 60, finger1_right_y - 175), (finger1_right_x, finger1_right_y + 25), (0,0,255), 3)
+                            cv2.rectangle(frame1, (finger1_right_x - 60, finger1_right_y - 175),
+                                          (finger1_right_x, finger1_right_y - 25), (0, 0, 255), 3)
+                    elif finger1_right_y - 75 < finger5_left_y < finger1_right_y - 50:
+                        if finger1_right_x - 30 < finger5_left_x < finger1_right_x + 30:
+                            gesture_text = 'ti'
+                            # cv2.rectangle(overlay, (finger1_right_x - 60, finger1_right_y - 175), (finger1_right_x, finger1_right_y + 25), (0,0,255), 3)
+                            cv2.rectangle(frame1, (finger1_right_x - 60, finger1_right_y - 175),
+                                          (finger1_right_x, finger1_right_y - 50), (0, 0, 255), 3)
+                    elif finger1_right_y - 125 < finger5_left_y < finger1_right_y - 75:
+                        if finger1_right_x - 30 < finger5_left_x < finger1_right_x + 30:
+                            gesture_text = 'do2'
+                            # cv2.rectangle(overlay, (finger1_right_x - 60, finger1_right_y - 175), (finger1_right_x, finger1_right_y + 25), (0,0,255), 3)
+                            cv2.rectangle(frame1, (finger1_right_x - 60, finger1_right_y - 175),
+                                          (finger1_right_x, finger1_right_y - 75), (0, 0, 255), 3)
+
 
         if control == 'p':
             gesture_text = 'Piano'
