e8fd3428a3bc91177511fdb78c4648a4fb20a217
JNU\dnflskfkakstp1183 <dnflskfkakstp1183@naver.com>
2023-12-16 02:57:59 +0900

    드럼 오류 해결

--- instrument.py
+++ instrument.py
@@ -217,145 +217,148 @@ with handsModule.Hands(static_image_mode=False, min_detection_confidence=0.7, mi
                                     cv2.rectangle(frame1, (finger1_right_x - 60, finger1_right_y - 175),
                                                   (finger1_right_x, finger1_right_y - 75), (0, 0, 255), 3)
 
+        if control == 'p':
+            gesture_text = 'Piano'
+            cv2.rectangle(overlay, (220, 490), (780, 810), (255, 255, 255), -1)
+            cv2.line(overlay, (220, 490), (780, 490), (0, 0, 0), 2)
+            cv2.line(overlay, (220, 490), (220, 810), (0, 0, 0), 2)
+            cv2.line(overlay, (220, 810), (780, 810), (0, 0, 0), 2)
+            cv2.line(overlay, (780, 490), (780, 810), (0, 0, 0), 2)
+            cv2.line(overlay, (300, 650), (300, 810), (0, 0, 0), 2)
+            cv2.line(overlay, (380, 650), (380, 810), (0, 0, 0), 2)
+            cv2.line(overlay, (460, 650), (460, 810), (0, 0, 0), 2)
+            cv2.line(overlay, (540, 650), (540, 810), (0, 0, 0), 2)
+            cv2.line(overlay, (620, 650), (620, 810), (0, 0, 0), 2)
+            cv2.line(overlay, (700, 650), (700, 810), (0, 0, 0), 2)
+            cv2.rectangle(overlay, (280, 490), (325, 650), (0, 0, 0), -1)
+            cv2.rectangle(overlay, (400, 490), (355, 650), (0, 0, 0), -1)
+            cv2.rectangle(overlay, (520, 490), (565, 650), (0, 0, 0), -1)
+            cv2.rectangle(overlay, (600, 490), (645, 650), (0, 0, 0), -1)
+            cv2.rectangle(overlay, (675, 490), (720, 650), (0, 0, 0), -1)
+            frame1 = cv2.addWeighted(overlay, 0.4, frame1, 1, 0)
+            if results.multi_hand_landmarks != None:
+                for handLandmarks in results.multi_hand_landmarks:
+                    drawingModule.draw_landmarks(frame1, handLandmarks, handsModule.HAND_CONNECTIONS)
+                    finger1_x = int(handLandmarks.landmark[8].x * 1000)
+                    finger1_y = int(handLandmarks.landmark[8].y * 1000)
 
-            if control == 'p':
-                gesture_text = 'Piano'
-                cv2.rectangle(overlay, (220, 490), (780, 810), (255, 255, 255), -1)
-                cv2.line(overlay, (220, 490), (780, 490), (0, 0, 0), 2)
-                cv2.line(overlay, (220, 490), (220, 810), (0, 0, 0), 2)
-                cv2.line(overlay, (220, 810), (780, 810), (0, 0, 0), 2)
-                cv2.line(overlay, (780, 490), (780, 810), (0, 0, 0), 2)
-                cv2.line(overlay, (300, 650), (300, 810), (0, 0, 0), 2)
-                cv2.line(overlay, (380, 650), (380, 810), (0, 0, 0), 2)
-                cv2.line(overlay, (460, 650), (460, 810), (0, 0, 0), 2)
-                cv2.line(overlay, (540, 650), (540, 810), (0, 0, 0), 2)
-                cv2.line(overlay, (620, 650), (620, 810), (0, 0, 0), 2)
-                cv2.line(overlay, (700, 650), (700, 810), (0, 0, 0), 2)
-                cv2.rectangle(overlay, (280, 490), (325, 650), (0, 0, 0), -1)
-                cv2.rectangle(overlay, (400, 490), (355, 650), (0, 0, 0), -1)
-                cv2.rectangle(overlay, (520, 490), (565, 650), (0, 0, 0), -1)
-                cv2.rectangle(overlay, (600, 490), (645, 650), (0, 0, 0), -1)
-                cv2.rectangle(overlay, (675, 490), (720, 650), (0, 0, 0), -1)
-                frame1 = cv2.addWeighted(overlay, 0.4, frame1, 1, 0)
-                if results.multi_hand_landmarks != None:
-                    for handLandmarks in results.multi_hand_landmarks:
-                        drawingModule.draw_landmarks(frame1, handLandmarks, handsModule.HAND_CONNECTIONS)
-                        finger1_x = int(handLandmarks.landmark[8].x * 1000)
-                        finger1_y = int(handLandmarks.landmark[8].y * 1000)
-
-
-                        if handLandmarks.landmark[7].y > handLandmarks.landmark[8].y:
-                            if 220 < finger1_x < 300 and 650 < finger1_y < 820:
-                                gesture_text = 'do'
-                                sound_dir = r'.\piano\p_do.wav'
-                                pygame.mixer.music.load(sound_dir)
-                                pygame.mixer.music.play()
-                            elif 280 < finger1_x < 325 and 650 > finger1_y > 490:
-                                gesture_text = 'do#'
-                                sound_dir = r'.\piano\p_dos.wav'
-                                pygame.mixer.music.load(sound_dir)
-                                pygame.mixer.music.play()
-                            elif 300 < finger1_x < 380 and 650 < finger1_y < 820:
-                                gesture_text = 're'
-                                sound_dir = r'.\piano\p_re.wav'
-                                pygame.mixer.music.load(sound_dir)
-                                pygame.mixer.music.play()
-                            elif 355 < finger1_x < 400 and 650 > finger1_y > 490:
-                                gesture_text = 're#'
-                                sound_dir = r'.\piano\p_res.wav'
-                                pygame.mixer.music.load(sound_dir)
-                                pygame.mixer.music.play()
-
-                            elif 380 < finger1_x < 460 and 650 < finger1_y < 820:
-                                gesture_text = 'mi'
-                                sound_dir = r'.\piano\p_mi.wav'
-                                pygame.mixer.music.load(sound_dir)
-                                pygame.mixer.music.play()
-
-                            elif 460 < finger1_x < 540 and 650 < finger1_y < 820:
-                                gesture_text = 'pa'
-                                sound_dir = r'.\piano\p_fa.wav'
-                                pygame.mixer.music.load(sound_dir)
-                                pygame.mixer.music.play()
-                            elif 520 < finger1_x < 565 and 650 > finger1_y > 490:
-                                gesture_text = 'pa#'
-                                sound_dir = r'.\piano\p_fas.wav'
-                                pygame.mixer.music.load(sound_dir)
-                                pygame.mixer.music.play()
-
-                            elif 540 < finger1_x < 620 and 650 < finger1_y < 820:
-                                gesture_text = 'sol'
-                                sound_dir = r'.\piano\p_sol.wav'
-                                pygame.mixer.music.load(sound_dir)
-                                pygame.mixer.music.play()
-                            elif 600 < finger1_x < 645 and 650 > finger1_y > 490:
-                                gesture_text = 'sol#'
-                                sound_dir = r'.\piano\p_sols.wav'
-                                pygame.mixer.music.load(sound_dir)
-                                pygame.mixer.music.play()
-
-                            elif 620 < finger1_x < 700 and 650 < finger1_y < 820:
-                                gesture_text = 'ra'
-                                sound_dir = r'.\piano\p_ra.wav'
-                                pygame.mixer.music.load(sound_dir)
-                                pygame.mixer.music.play()
-                            elif 675 < finger1_x < 720 and 650 > finger1_y > 490:
-                                gesture_text = 'ra#'
-                                sound_dir = r'.\piano\p_ras.wav'
-                                pygame.mixer.music.load(sound_dir)
-                                pygame.mixer.music.play()
-
-                            elif 700 < finger1_x < 780 and 650 < finger1_y < 820:
-                                gesture_text = 'ti'
-                                sound_dir = r'.\piano\p_si.wav'
-                                pygame.mixer.music.load(sound_dir)
-                                pygame.mixer.music.play()
-
-            if control == 'd':
-                gesture_text = 'Drum'
-                cv2.line(overlay, (0, 350), (1000, 350), (0, 0, 0), 2)
-                frame1 = cv2.addWeighted(overlay, 0.4, frame1, 1, 0)
-                if results.multi_hand_landmarks != None:
-                    for handLandmarks in results.multi_hand_landmarks:
-                        drawingModule.draw_landmarks(frame1, handLandmarks, handsModule.HAND_CONNECTIONS)
-                        finger1_x = int(handLandmarks.landmark[4].x * 1000)
-                        finger1_y = int(handLandmarks.landmark[4].y * 1000)
-
-                        if 0 < finger1_x < 400 and 0 < finger1_y < 400:
-                            gesture_text = 'leftcymbal'
-                            sound_dir = r'.\drum\leftcymbal.wav'
+                    if handLandmarks.landmark[7].y > handLandmarks.landmark[8].y:
+                        if 220 < finger1_x < 300 and 650 < finger1_y < 820:
+                            gesture_text = 'do'
+                            sound_dir = r'.\piano\p_do.wav'
                             pygame.mixer.music.load(sound_dir)
                             pygame.mixer.music.play()
-                        elif 450 < finger1_x < 550 and 0 < finger1_y < 400:
-                            gesture_text = 'lefttang'
-                            sound_dir = r'.\drum\lefttang.wav'
+                        elif 280 < finger1_x < 325 and 650 > finger1_y > 490:
+                            gesture_text = 'do#'
+                            sound_dir = r'.\piano\p_dos.wav'
                             pygame.mixer.music.load(sound_dir)
                             pygame.mixer.music.play()
-                        elif 600 < finger1_x < 700 and 0 < finger1_y < 400:
-                            gesture_text = 'righttang'
-                            sound_dir = r'.\drum\righttang.wav'
+                        elif 300 < finger1_x < 380 and 650 < finger1_y < 820:
+                            gesture_text = 're'
+                            sound_dir = r'.\piano\p_re.wav'
                             pygame.mixer.music.load(sound_dir)
                             pygame.mixer.music.play()
-                        elif 750 < finger1_x < 1500 and 0 < finger1_y < 400:
-                            gesture_text = 'rightcybal'
-                            sound_dir = r'.\drum\rightcymbal.wav'
+                        elif 355 < finger1_x < 400 and 650 > finger1_y > 490:
+                            gesture_text = 're#'
+                            sound_dir = r'.\piano\p_res.wav'
                             pygame.mixer.music.load(sound_dir)
                             pygame.mixer.music.play()
-                        elif 0 < finger1_x < 350 and 500 < finger1_y < 1000:
-                            gesture_text = 'leftdrum'
-                            sound_dir = r'.\drum\leftdrum.wav'
+
+                        elif 380 < finger1_x < 460 and 650 < finger1_y < 820:
+                            gesture_text = 'mi'
+                            sound_dir = r'.\piano\p_mi.wav'
                             pygame.mixer.music.load(sound_dir)
                             pygame.mixer.music.play()
-                        elif 400 < finger1_x < 650 and 500 < finger1_y < 1000:
-                            gesture_text = 'base'
-                            sound_dir = r'.\drum\base.wav'
+
+                        elif 460 < finger1_x < 540 and 650 < finger1_y < 820:
+                            gesture_text = 'pa'
+                            sound_dir = r'.\piano\p_fa.wav'
                             pygame.mixer.music.load(sound_dir)
                             pygame.mixer.music.play()
-                        elif 700 < finger1_x < 1000 and 500 < finger1_y < 1000:
-                            gesture_text = 'rightdrum'
-                            sound_dir = r'.\drum\rightdrum.wav'
+                        elif 520 < finger1_x < 565 and 650 > finger1_y > 490:
+                            gesture_text = 'pa#'
+                            sound_dir = r'.\piano\p_fas.wav'
                             pygame.mixer.music.load(sound_dir)
                             pygame.mixer.music.play()
+
+                        elif 540 < finger1_x < 620 and 650 < finger1_y < 820:
+                            gesture_text = 'sol'
+                            sound_dir = r'.\piano\p_sol.wav'
+                            pygame.mixer.music.load(sound_dir)
+                            pygame.mixer.music.play()
+                        elif 600 < finger1_x < 645 and 650 > finger1_y > 490:
+                            gesture_text = 'sol#'
+                            sound_dir = r'.\piano\p_sols.wav'
+                            pygame.mixer.music.load(sound_dir)
+                            pygame.mixer.music.play()
+
+                        elif 620 < finger1_x < 700 and 650 < finger1_y < 820:
+                            gesture_text = 'ra'
+                            sound_dir = r'.\piano\p_ra.wav'
+                            pygame.mixer.music.load(sound_dir)
+                            pygame.mixer.music.play()
+                        elif 675 < finger1_x < 720 and 650 > finger1_y > 490:
+                            gesture_text = 'ra#'
+                            sound_dir = r'.\piano\p_ras.wav'
+                            pygame.mixer.music.load(sound_dir)
+                            pygame.mixer.music.play()
+
+                        elif 700 < finger1_x < 780 and 650 < finger1_y < 820:
+                            gesture_text = 'ti'
+                            sound_dir = r'.\piano\p_si.wav'
+                            pygame.mixer.music.load(sound_dir)
+                            pygame.mixer.music.play()
+
+        if control == 'd':
+            gesture_text = 'Drum'
+            cv2.line(overlay, (0, 350), (1000, 350), (0, 0, 0), 2)
+            cv2.line(overlay, (250, 0), (250, 350), (0, 0, 0), 2)
+            cv2.line(overlay, (500, 0), (500, 350), (0, 0, 0), 2)
+            cv2.line(overlay, (750, 0), (750, 350), (0, 0, 0), 2)
+            cv2.line(overlay, (350, 350), (350, 1000), (0, 0, 0), 2)
+            cv2.line(overlay, (750, 350), (750, 1000), (0, 0, 0), 2)
+            frame1 = cv2.addWeighted(overlay, 0.4, frame1, 1, 0)
+            if results.multi_hand_landmarks != None:
+                for handLandmarks in results.multi_hand_landmarks:
+                    drawingModule.draw_landmarks(frame1, handLandmarks, handsModule.HAND_CONNECTIONS)
+                    finger1_x = int(handLandmarks.landmark[4].x * 1000)
+                    finger1_y = int(handLandmarks.landmark[4].y * 1000)
+
+                    if 0 < finger1_x < 250 and 0 < finger1_y < 400:
+                        gesture_text = 'leftcymbal'
+                        sound_dir = r'.\drum\leftcymbal.wav'
+                        pygame.mixer.music.load(sound_dir)
+                        pygame.mixer.music.play()
+                    elif 270 < finger1_x < 500 and 0 < finger1_y < 400:
+                        gesture_text = 'lefttang'
+                        sound_dir = r'.\drum\lefttang.wav'
+                        pygame.mixer.music.load(sound_dir)
+                        pygame.mixer.music.play()
+                    elif 520 < finger1_x < 750 and 0 < finger1_y < 400:
+                        gesture_text = 'righttang'
+                        sound_dir = r'.\drum\righttang.wav'
+                        pygame.mixer.music.load(sound_dir)
+                        pygame.mixer.music.play()
+                    elif 770 < finger1_x < 1500 and 0 < finger1_y < 400:
+                        gesture_text = 'rightcybal'
+                        sound_dir = r'.\drum\rightcymbal.wav'
+                        pygame.mixer.music.load(sound_dir)
+                        pygame.mixer.music.play()
+                    elif 0 < finger1_x < 350 and 500 < finger1_y < 1000:
+                        gesture_text = 'leftdrum'
+                        sound_dir = r'.\drum\leftdrum.wav'
+                        pygame.mixer.music.load(sound_dir)
+                        pygame.mixer.music.play()
+                    elif 370 < finger1_x < 650 and 500 < finger1_y < 1000:
+                        gesture_text = 'base'
+                        sound_dir = r'.\drum\base.wav'
+                        pygame.mixer.music.load(sound_dir)
+                        pygame.mixer.music.play()
+                    elif 670 < finger1_x < 1000 and 500 < finger1_y < 1000:
+                        gesture_text = 'rightdrum'
+                        sound_dir = r'.\drum\rightdrum.wav'
+                        pygame.mixer.music.load(sound_dir)
+                        pygame.mixer.music.play()
 
         cv2.putText(frame1, text='name: {}'.format(gesture_text)
                     , org=(10, 30), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
